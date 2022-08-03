f7529a4242e2afcde406afafd3732d39e9950bdb
unknown <dhckdgns3@naver.com>
2022-08-03 23:22:30 +0900

    add: file upload method & save database

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -3,10 +3,12 @@ import {
   Controller,
   InternalServerErrorException,
   Post,
+  Req,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Request } from 'express';
 import { FileType } from './file.interface';
 import { FileService } from './file.service';
 
@@ -18,13 +20,22 @@ export class FileController {
   }
   @Post()
   @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: FileType) {
+  async uploadFile(@UploadedFile() file: FileType, @Req() req: Request) {
     if (file === undefined || file === null) {
       throw new BadRequestException();
     }
 
+    const protocol =
+      req.headers['X-Forwarded-Proto'] ??
+      req.headers['x-forwared-proto'] ??
+      req.protocol;
+    const hostname = req.headers.host ?? req.hostname;
+
     try {
-      return await this.#fileService.uploadFile(file);
+      return await this.#fileService.uploadFile(
+        file,
+        `${protocol}://${hostname}`,
+      );
     } catch (e) {
       throw new InternalServerErrorException();
     }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -6,6 +6,7 @@ import { FileType } from './file.interface';
 import * as crypto from 'crypto';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as stream from 'stream/promises';
 
 @Injectable()
 export class FileService {
@@ -19,11 +20,37 @@ export class FileService {
       fs.mkdirSync(uploadPath);
     }
   }
-  async uploadFile(file: FileType) {
-    const extname = path.extname(file.originalname).toLowerCase().substring(1);
+  async uploadFile(multerFile: FileType, hostname: string, priority = 0) {
+    const extname = path
+      .extname(multerFile.originalname)
+      .toLowerCase()
+      .substring(1);
     const tempName = `${crypto.randomBytes(16).toString('hex')}.${extname}`;
     const tempPath = path.join(process.cwd(), '/upload', tempName);
+    await fs.promises.writeFile(tempPath, multerFile.buffer);
 
-    await fs.promises.writeFile(tempPath, file.buffer);
+    const hash = crypto.createHash('md5');
+    await stream.pipeline(fs.createReadStream(tempPath), hash);
+    const md5 = hash.digest('hex');
+
+    const targetPath = path.join(process.cwd(), '/upload', `${md5}.${extname}`);
+    if (fs.existsSync(targetPath) === false) {
+      await fs.promises.rename(tempPath, targetPath);
+    } else {
+      fs.promises.rm(tempPath);
+    }
+
+    const size = (await fs.promises.stat(targetPath)).size;
+
+    const file = await this.#fileRepository.create({
+      filename: multerFile.originalname,
+      mimetype: multerFile.mimetype,
+      size: size,
+      md5: md5,
+      priority: priority,
+      url: new URL(path.join('/upload', `${md5}.${extname}`), hostname).href,
+    });
+
+    return await this.#fileRepository.save(file);
   }
 }
